8b5d2f39237d5a30b883f315bf1663a03ed4dc22
Dmitry <okyn.dima@yandex.ru>
2025-04-11 22:44:08 +0300

    Дополнены заголовочные файлы user.h chat.h, дополнен README, изменена логика хранения сообщений

--- include/chat.h
+++ include/chat.h
@@ -1,15 +1,45 @@
 #pragma once
+#include "user.h"
 #include <string>
 #include <vector>
 #include <memory>
+#include <utility>
 
+/*
+Chat - абстрактный класс, наследники ChatPrivate, ChatHared
+*/
 class Chat
 {
-private:
-    /* data */
+private:  
+    /*
+        historyChat - состоит из списка пар 
+            pair - должен содержать указатель на пользователя и само сообщение
+            pair  в последующем можно заменить на tuple, если будет нужно больше полей, например, дата/время
+        
+        .first - weak_ptr<User>
+        .second - string Message
+    */
+    std::vector<std::pair<std::weak_ptr<User>, std::string>> historyChat;
+    //Список участников
+    std::vector<std::weak_ptr<User>> usersInChat;
+
 public:
-    Chat(/* args */);
-    ~Chat();
+    virtual ~Chat() = default;
+    
+    // Должен передать количество участников в ChatHared или имя пользователя
+    // добавить дополнительные поля для классов - счетчик для ChatHared, ptr для ChatPrivate
+    // !!! проверять на пустоту historyChat в функцииt, проверять на пустоту string при вызове
+    virtual std::string showUsers();
+
+    // Должен предать все сообщения из historyChat, форматируя текст относительно пользователя User, тоесть того, кто открыл чат
+    // !!! проверять на пустоту historyChat в функцииt, проверять на пустоту string при вызове
+    virtual std::string getOllMessage(std::weak_ptr<User>);
+
+    // Указатель взять из dataBace shared_ptr и из него сделать weak_ptr
+    // Добавить пользователя в чат общий или при личных сообщениях
+    // Добавить проверку на ChatPrivate, если в нем уже 2, то отклонить заявку
+    // вернет true - при успехе, false - при ошибке
+    bool setUsersInChat(std::weak_ptr<User>);
 };
 
 
--- include/user.h
+++ include/user.h
@@ -6,22 +6,37 @@
 
 /*
 Класс User взаимодействует с классами:
-        Database                - обмен данными авторизации, обмен данными
-        Chat -> ChatPrivate     - объект User хранит указатели на чаты, что обеспечивает доступ к уже имеющимся приватным чатам
-                -> ChatHared    - объект User хранит указатели на чаты, что обеспечивает доступ к уже имеющимся общим чатам
+        Database                - обмен данными авторизации, должен хранить указатели на всех пользователей shared_ptr нужен в том числе для преобразования 
+                                        в weak_ptr в классах Chat
+        Chat    -> ChatPrivate     - объект User хранит указатели на чаты, что обеспечивает доступ к уже имеющимся приватным чатам Хранит. историю сообщений.
+                -> ChatHared    - объект User хранит указатели на чаты, что обеспечивает доступ к уже имеющимся общим чатам. Хранит историю сообщений.
         Autorization            - Autorization создает объект данного класса, или осуществляет логирование пользователя по уже имеющимся объектам User
 */
-
 class User
 {
 private:
-    unsigned int id;
     std::string _login;
     std::string _pasword;
     std::string _name;
     std::vector<std::shared_ptr<Chat>> _connectionChatId;
 public:
+    // Передать логин, пароль, имя
     User(std::string login, std::string pasword, std::string name);
+    
+    // Получить логин
+    std::string getLogin() const;
+
+    // Получить имя
+    std::string getName() const;
+
+    // Получить список чатов пользователя
+    std::vector<std::shared_ptr<Chat>> getConnectionChatId() const;
+
+    // Добавить пользователя в чат общий или при личных сообщениях
+    // Добавить проверку на ChatPrivate, если в нем уже 2, то отклонить заявку
+    // вернет true - при успехе, false - при ошибке
+    bool setChat(std::shared_ptr<Chat>);
+
     ~User();
 };
 
